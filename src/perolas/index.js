91f9fc3ac9a7d4526bc0df5887650e2126cb3457
gracione <gracione2017@gmail.com>
2022-08-21 14:25:20 -0300

    perolas

--- src/perolas/index.js
+++ src/perolas/index.js
@@ -1,22 +1,23 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../services/api';
-
-import './styles.css';
+import { Card, Header } from './styles';
 
 export default function Logon() {
-  const [user, setUser] = useState();
+    const [user, setUser] = useState();
 
-  useEffect(() => {
-    api
-      .get()
-      .then((response) => setUser(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
-  return (
-    <>
-    {user?.quote}
-    </>
-  );
+    useEffect(() => {
+        api
+            .get()
+            .then((response) => setUser(response.data))
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }, []);
+    return (
+        <Header>
+            <Card>
+                {user?.quote}
+            </Card>
+        </Header>
+    );
 }
---
+++ src/perolas/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Card = styled.div`
+    border-width: 20px;
+    border-style: double;
+    border-color: #030;
+    opacity: 0.8;
+    margin-top:250px;
+    font-size:40px;
+
+    display:flex;
+    justify-content: center;
+    `
+
+export const Header = styled.div`
+    width:100%;
+    background: no-repeat url(fundo.jpg);
+
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    `

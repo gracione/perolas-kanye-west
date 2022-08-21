92e81d4837a74c2599758b73f9ba2788e15d24f8
gracione <gracione2017@gmail.com>
2022-08-21 12:11:37 -0300

    perolas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Logon from './paginas/Autenticacao/Login/index';
-import Home from './paginas/Home/index';
-import Registrar from './paginas/Autenticacao/Registrar/index';
-import Funcionarios from './paginas/Funcionarios';
+import Perolas from './perolas/index';
 
 export default function Rota() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Logon />} />
-                <Route path="/register" element={<Registrar />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/funcionarios" element={<Funcionarios />} />
+                <Route path="/" element={<Perolas/>} />
             </Routes>
         </BrowserRouter>
     );
